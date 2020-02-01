32a3da7c92925d1bf5d6d8c915cfc62f23a30c0e
Trevor Atwijukire <44920930+TrevorNathan@users.noreply.github.com>
2020-02-01 22:05:02 +0300

    Add files via upload
    Assignment week-6 has 2 file

---
+++ forcomp/package.scala
@@ -0,0 +1,44 @@
+package object forcomp {
+
+  //val dictionaryPath = List("forcomp", "linuxwords.txt")
+
+  def loadDictionary = {
+    val wordstream = Option {
+      getClass.getResourceAsStream(List("forcomp", "linuxwords.txt").mkString("/", "/", ""))
+    } getOrElse {
+      sys.error("Could not load word list, dictionary file not found")
+    }
+    try {
+      val s = io.Source.fromInputStream(wordstream)
+      s.getLines.toList
+    } catch {
+      case e: Exception =>
+        println("Could not load word list: " + e)
+        throw e
+    } finally {
+      wordstream.close()
+    }
+  }
+}
+
+/*
+object Dictionary {
+  def loadDictionary: List[String] = {
+    val wordstream = Option {
+      getClass.getResourceAsStream(List("forcomp", "linuxwords.txt").mkString("/", "/", ""))
+    } getOrElse {
+      sys.error("Could not load word list, dictionary file not found")
+    }
+    try {
+      val s = scala.io.Source.fromInputStream(wordstream)
+      s.getLines.toList
+    } catch {
+      case e: Exception =>
+        println("Could not load word list: " + e)
+        throw e
+    } finally {
+      wordstream.close()
+    }
+  }
+}
+ */
